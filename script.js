0b154256553db5de45b57ce6069cb0d9b32928b3
DavidVillarM <144356845+DavidVillarM@users.noreply.github.com>
2023-09-16 09:02:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const cantidad = parseFloat(document.getElementById("cantidad").value);
+const 
+
+/*function convert() {
+    // Obtener valores del usuario
+    const amount = parseFloat(document.getElementById("amount").value);
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
+
+    // Realizar la conversión (puedes usar tasas de cambio en tiempo real aquí)
+    // Por simplicidad, asumiremos una tasa fija para la conversión
+    const exchangeRate = 1.2; // Ejemplo: 1 USD = 1.2 EUR
+
+    // Calcular el resultado
+    const result = amount * exchangeRate;
+
+    // Mostrar el resultado en la página
+    document.getElementById("result").textContent = "Resultado: " + result.toFixed(2) + " " + toCurrency;
+}*/
